eebf1ad44835dba0300b8afd3ce11bd883f5d118
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-07-25 09:37:47 +0200

    Change method lastRFID to return immediately and have nextRFID method to return promise

--- osgi.enroute.trains.api/src/osgi/enroute/trains/controller/api/RFIDSegmentController.java
+++ osgi.enroute.trains.api/src/osgi/enroute/trains/controller/api/RFIDSegmentController.java
@@ -8,8 +8,13 @@ import org.osgi.util.promise.Promise;
 public interface RFIDSegmentController extends SegmentController {
 
 	/**
+	 * Return the last seen RFID
+	 */
+	String lastRFID();
+	
+	/**
 	 * Read an RFID. Resolves the promise when a new RFID is read.
 	 * @return
 	 */
-	Promise<String> lastRFID();
+	Promise<String> nextRFID();
 }
