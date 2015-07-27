66553e886ab983d9d6871073bb021759ae682362
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-07-27 15:53:14 +0200

    [trains] add example track manager project

---
+++ osgi.enroute.trains.track.manager.example.provider/readme.md
@@ -0,0 +1,15 @@
+# OSGI ENROUTE TRAINS TRACK MANAGER EXAMPLE PROVIDER
+
+${Bundle-Description}
+
+## Example
+
+## Configuration
+
+	Pid: osgi.enroute.trains.track.manager.example
+	
+	Field					Type				Description
+		
+	
+## References
+
---
+++ osgi.enroute.trains.track.manager.example.provider/src/osgi/enroute/trains/track/manager/example/provider/ExampleTrackManagerImpl.java
@@ -0,0 +1,11 @@
+package osgi.enroute.trains.track.manager.example.provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ */
+@Component(name = "osgi.enroute.trains.track.manager.example")
+public class ExampleTrackManagerImpl {
+
+}
