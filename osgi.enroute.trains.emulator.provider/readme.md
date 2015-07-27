532b09bd671a91eec713865a73ed97979f83003f
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-07-27 15:48:32 +0200

    [trains] add emulator project

---
+++ osgi.enroute.trains.emulator.provider/readme.md
@@ -0,0 +1,15 @@
+# OSGI ENROUTE TRAINS EMULATOR PROVIDER
+
+${Bundle-Description}
+
+## Example
+
+## Configuration
+
+	Pid: osgi.enroute.trains.emulator
+	
+	Field					Type				Description
+		
+	
+## References
+
---
+++ osgi.enroute.trains.emulator.provider/src/osgi/enroute/trains/emulator/provider/Emulator.java
@@ -0,0 +1,11 @@
+package osgi.enroute.trains.emulator.provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ */
+@Component(name = "osgi.enroute.trains.emulator")
+public class Emulator {
+
+}
