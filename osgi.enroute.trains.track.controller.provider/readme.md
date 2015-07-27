982a0427c087988fa5f83e0073174915f906e0e9
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-07-27 15:51:23 +0200

    [trains] add track controller project

---
+++ osgi.enroute.trains.track.controller.provider/readme.md
@@ -0,0 +1,15 @@
+# OSGI ENROUTE TRAINS TRACK CONTROLLER PROVIDER
+
+${Bundle-Description}
+
+## Example
+
+## Configuration
+
+	Pid: osgi.enroute.trains.track.controller
+	
+	Field					Type				Description
+		
+	
+## References
+
---
+++ osgi.enroute.trains.track.controller.provider/src/osgi/enroute/trains/track/controller/provider/TrackControllerImpl.java
@@ -0,0 +1,11 @@
+package osgi.enroute.trains.track.controller.provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ */
+@Component(name = "osgi.enroute.trains.track.controller")
+public class TrackControllerImpl {
+
+}
