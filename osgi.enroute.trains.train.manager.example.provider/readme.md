3ce4a22afb7b02006ecfb36d85e144892fe12af2
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-07-27 15:53:52 +0200

    [trains] add example train manager project

---
+++ osgi.enroute.trains.train.manager.example.provider/readme.md
@@ -0,0 +1,15 @@
+# OSGI ENROUTE TRAINS TRAIN MANAGER EXAMPLE PROVIDER
+
+${Bundle-Description}
+
+## Example
+
+## Configuration
+
+	Pid: osgi.enroute.trains.train.manager.example
+	
+	Field					Type				Description
+		
+	
+## References
+
---
+++ osgi.enroute.trains.train.manager.example.provider/src/osgi/enroute/trains/train/manager/example/provider/ExampleTrainManagerImpl.java
@@ -0,0 +1,11 @@
+package osgi.enroute.trains.train.manager.example.provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ */
+@Component(name = "osgi.enroute.trains.train.manager.example")
+public class ExampleTrainManagerImpl {
+
+}
