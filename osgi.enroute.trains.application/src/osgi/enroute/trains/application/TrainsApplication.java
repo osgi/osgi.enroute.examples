15654393fdde7b6accb23fbee43ec91828d5da9a
Peter Kriens <peter.kriens@aqute.biz>
2015-07-30 17:43:40 +0200

    [trains] The app
    Created a simple app that only works with text now. It gets the segments from the server, it receives the events and shows where the train is. No graphics yet

--- osgi.enroute.trains.application/src/osgi/enroute/trains/application/TrainsApplication.java
+++ osgi.enroute.trains.application/src/osgi/enroute/trains/application/TrainsApplication.java
@@ -1,25 +1,52 @@
 package osgi.enroute.trains.application;
 
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import osgi.enroute.capabilities.AngularWebResource;
 import osgi.enroute.capabilities.BootstrapWebResource;
 import osgi.enroute.capabilities.ConfigurerExtender;
 import osgi.enroute.capabilities.EasseWebResource;
+import osgi.enroute.capabilities.JsonrpcWebResource;
 import osgi.enroute.capabilities.WebServerExtender;
-import osgi.enroute.rest.api.REST;
-import osgi.enroute.rest.api.RESTRequest;
+import osgi.enroute.jsonrpc.api.JSONRPC;
+import osgi.enroute.trains.cloud.api.Segment;
+import osgi.enroute.trains.cloud.api.TrackInfo;
 
 @AngularWebResource(resource={"angular.js","angular-resource.js", "angular-route.js"}, priority=1000)
 @BootstrapWebResource(resource="css/bootstrap.css")
 @WebServerExtender
 @ConfigurerExtender
 @EasseWebResource
-@Component(name="osgi.enroute.trains")
-public class TrainsApplication implements REST {
+@JsonrpcWebResource
+@Component(name="osgi.enroute.trains", property=JSONRPC.ENDPOINT + "=trains")
+public class TrainsApplication implements JSONRPC {
 
-	public String getUpper(RESTRequest rq, String string) {
-		return string.toUpperCase();
+	private TrackInfo ti;
+
+	public Map<String, Segment> getSegments() {
+		security();
+		return ti.getSegments();
+	}
+
+	private void security() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getDescriptor() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	@Reference
+	void setTrackInfo( TrackInfo ti) {
+		this.ti = ti;
+		
 	}
 
 }
--- osgi.enroute.trains.application/web/main.js
+++ osgi.enroute.trains.application/web/main.js
@@ -2,42 +2,97 @@
 
 (function() {
 
-	var MODULE = angular.module('osgi.enroute.trains',
-			[ 'ngRoute', 'ngResource', 'enEasse' ]);
+	var MODULE = angular.module('osgi.enroute.trains', [ 'ngRoute',
+			'ngResource', 'enJsonrpc', 'enEasse' ]);
 
-	MODULE.config( function($routeProvider) {
-		$routeProvider.when('/', { controller: mainProvider, templateUrl: '/osgi.enroute.trains/main/htm/home.htm'});
-		$routeProvider.when('/about', { templateUrl: '/osgi.enroute.trains/main/htm/about.htm'});
+	var resolveBefore;
+	var alerts = [];
+	var trains = {
+		events : [],
+		segments : {},
+		locations: {},
+		ep : null
+	};
+
+	function error(msg) {
+		alerts.push({
+			msg : msg,
+			type : 'danger'
+		});
+	}
+
+	MODULE.config(function($routeProvider, en$jsonrpcProvider) {
+		en$jsonrpcProvider.setNotification({
+			error : error
+		})
+		resolveBefore = {
+			trainsEndpoint : en$jsonrpcProvider.endpoint("trains")
+		};
+
+		$routeProvider.when('/', {
+			controller : mainProvider,
+			templateUrl : '/osgi.enroute.trains/main/htm/home.htm',
+			resolve : resolveBefore
+		});
+		$routeProvider.when('/about', {
+			templateUrl : '/osgi.enroute.trains/main/htm/about.htm',
+			resolve : resolveBefore
+		});
 		$routeProvider.otherwise('/');
+
 	});
-	
-	MODULE.run( function($rootScope, $location) {
-		$rootScope.alerts = [];
+
+	MODULE.run(function($rootScope, $location, en$easse, en$jsonrpc) {
+
+		$rootScope.alerts = alerts;
+		$rootScope.trains = trains;
+
+		en$easse.handle("osgi/trains/*", function(e) {
+			$rootScope.$applyAsync(function() {
+				trains.events.push(e);
+				if ( trains.events.length > 10)
+					trains.events.splice(0,1);
+				if ( e.type == "LOCATED") {
+					trains.locations[e.train]=e.segment;
+				}
+			});
+		}, function(e) {
+			alerts.push({
+				type : 'error',
+				msg : e
+			});
+		});
+
+		resolveBefore.trainsEndpoint().then(function(ep) {
+			trains.ep = ep;
+			ep.getSegments().then( function(d) {
+				for ( var k in d ) {
+					var seg = d[k];
+					seg.trains = [];
+					switch(seg.type) {
+					case "SIGNAL":
+						seg.signal = "red";
+						break;
+						
+					case "SWITCH":
+						seg.alternative = false;
+						break;
+					}
+				}
+				trains.segments = d;
+			});
+		});
+
 		$rootScope.closeAlert = function(index) {
-			$rootScope.alerts.splice(index, 1);
+			alerts.splice(index, 1);
 		};
 		$rootScope.page = function() {
 			return $location.path();
 		}
+
 	});
-	
-	
-	
-	var mainProvider = function($scope, $http) {
-		
-		$scope.upper = function() {
-			var name = prompt("Under what name?");
-			if ( name ) {
-				$http.get('/rest/upper/'+name).then(
-						function(d) {
-							$scope.alerts.push( { type: 'success', msg: d.data });
-						}, function(d) {
-							$scope.alerts.push( { type: 'danger', msg: 'Failed with ['+ d.status + '] '+ d.statusText });
-						}
-				);
-			}
-		};
-	
+
+	var mainProvider = function($scope) {
 	}
-	
+
 })();
