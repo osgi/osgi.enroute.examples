9c5abf157f31c81f2da176e2d82e2eb0f7942f36
Peter Kriens <peter.kriens@aqute.biz>
2015-10-19 16:06:21 +0200

    Added a simple test project

---
+++ osgi.enroute.examples.test/src/osgi/enroute/examples/test/ExamplesTest.java
@@ -0,0 +1,23 @@
+package osgi.enroute.examples.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * 
+ */
+
+public class ExamplesTest {
+
+    private final BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+    
+    /*
+     * 
+     */
+    @Test
+    public void testExamples() throws Exception {
+    	Assert.assertNotNull(context);
+    }
+}
