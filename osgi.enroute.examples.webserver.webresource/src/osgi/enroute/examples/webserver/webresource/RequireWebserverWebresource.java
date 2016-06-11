948aaa599c836aa747bc92e02bbaad29ad941c55
Paul F Fraser <paulf@a2zliving.com>
2016-06-12 09:49:35 +1000

    Add explanation to bnd description so user knows to type in wifi
    Signed-off-by: Paul F Fraser <paulf@a2zliving.com>

--- osgi.enroute.examples.webserver.webresource/src/osgi/enroute/examples/webserver/webresource/RequireWebserverWebresource.java
+++ osgi.enroute.examples.webserver.webresource/src/osgi/enroute/examples/webserver/webresource/RequireWebserverWebresource.java
@@ -6,7 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import aQute.bnd.annotation.headers.RequireCapability;
 import osgi.enroute.namespace.WebResourceNamespace;
 
-@RequireCapability(ns = WebResourceNamespace.NS, filter = "(&(" + WebResourceNamespace.NS + "="
+@RequireCapability(
+		ns = WebResourceNamespace.NS, 
+		filter = "(&(" + WebResourceNamespace.NS + "="
 		+ WebserverResourceConstants.WEBSERVER_WEB_RESOURCE_PATH + ")${frange;" + WebserverResourceConstants.WEBSERVER_WEB_RESOURCE_VERSION
 		+ "})")
 @Retention(RetentionPolicy.CLASS)
